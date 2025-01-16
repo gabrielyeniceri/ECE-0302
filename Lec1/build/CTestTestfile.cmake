205c60abd877e32a049cdc8d629cf2a537c08972
Gabriel Yeniceri <130937135+gabrielyeniceri@users.noreply.github.com>
2025-01-16 14:46:59 -0500

    upload

---
+++ Lec1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/gkyen/OneDrive/Documents/ece0302/Lec1
+# Build directory: C:/Users/gkyen/OneDrive/Documents/ece0302/Lec1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-boxes "test_boxes")
+set_tests_properties(test-boxes PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/gkyen/OneDrive/Documents/ece0302/Lec1/CMakeLists.txt;19;add_test;C:/Users/gkyen/OneDrive/Documents/ece0302/Lec1/CMakeLists.txt;0;")
