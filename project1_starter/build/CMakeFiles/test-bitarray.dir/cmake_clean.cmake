377a32cae4e76e107b4448173c8d46357ee1c343
Gabriel Yeniceri <130937135+gabrielyeniceri@users.noreply.github.com>
2025-01-16 14:51:37 -0500

    upload p1

---
+++ project1_starter/build/CMakeFiles/test-bitarray.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-bitarray.dir/bitarray.cpp.obj"
+  "CMakeFiles/test-bitarray.dir/bitarray.cpp.obj.d"
+  "CMakeFiles/test-bitarray.dir/test_bitarray.cpp.obj"
+  "CMakeFiles/test-bitarray.dir/test_bitarray.cpp.obj.d"
+  "libtest-bitarray.dll.a"
+  "test-bitarray.exe"
+  "test-bitarray.exe.manifest"
+  "test-bitarray.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test-bitarray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
